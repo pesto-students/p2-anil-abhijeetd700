f098e7e641f8b99d54869bfa0f3be94012e645df
Abhijeet Deshmukh <abhijeetd700@gmail.com>
2022-08-20 09:55:15 +0530

    Implemented API & final Integration

--- Week-14/financeApp/app.js
+++ Week-14/financeApp/app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose= require('mongoose');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+let mongoConnUrl = "mongodb://localhost/finance"
+mongoose.connect(mongoConnUrl,{useNewUrlParser:true});
+let db = mongoose.connection;
+
+db.on('error',function(error){
+  console.log('Error came in connecting'+error)
+})
+db.on('open',function(){
+  console.log('YEs we are connected to mongodb & the database')
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +30,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- Week-14/financeApp/routes/users.js
+++ Week-14/financeApp/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Users')
-});
+let userController = require('../controllers/users')
+
+router.post('/login',userController.loginUser);
+router.post('/register',userController.registerUser);
+router.get('/info',userController.showInfo);
+router.delete('/:id',userController.deleteUser)
+router.put('/:id',userController.editUser)
+router.post('/:year',userController.filterByYear)
+router.post('/by/:month',userController.filterByMonth)
 
 module.exports = router;
